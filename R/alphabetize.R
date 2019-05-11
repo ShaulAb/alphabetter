104390cf23ed6b110d791700184d8f8388967154
shaul <shaul@datascience.co.il>
2019-05-11 20:43:38 +0300

    initial commit - v0.0.1

---
+++ R/alphabetize.R
@@ -0,0 +1,33 @@
+#' Sort Selected Lines
+#'
+#' Call this addin for code sections like libraries, constants etc.
+#'
+#' @export
+#'
+alphabetize <- function() {
+
+  context <- rstudioapi::getActiveDocumentContext()
+  my_sel <- unlist(context$selection)["text"]
+  rstudioapi::insertText(alpha_sort(my_sel))
+}
+
+
+#' Sort by Alphabet
+#'
+#' @description helper function for alphabetize addin.
+#' Given active text selection, the function parses the text and
+#' re-order them by the alphabet.
+#'
+#' @param text String, corresponds to user text selection
+#'
+#' @return sorted text
+#' @export
+#'
+alpha_sort <- function(text) {
+
+  # split by newline ('\n')
+  words <- stringr::str_split(text, pattern = "\n", simplify = TRUE)
+
+  # sort alphabetically and collapse back by newline
+  paste(sort(words), collapse = "\n")
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# alphabetter
+
+The goal of alphabetter is to easily sort code sections alphabetically.
+
+## Example
+
+In my code I always have `setups` section which includes imports and constants.
+
+``` r
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+
+FIG_WIDTH = 750L
+DATA_PATH = "./data"
+```
+
+You can alphabetize the above code by selecting relevant lines and apply the addin
+
+``` r
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+
+DATA_PATH = "./data"
+FIG_WIDTH = 750L
+```
