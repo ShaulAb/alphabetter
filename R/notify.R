543c7ce1ccbc95462c1eec7be54186886fdd71bf
shaul <shaul@datascience.co.il>
2019-10-12 19:18:32 +0300

    notify on used/loaded libraries

--- R/notify.R
+++ R/notify.R
@@ -11,6 +11,19 @@ notify_io_addin <- function() {
   rstudioapi::insertText(notify_io(text))
 }
 
+#' Notify On Used/Loaded Libraries.
+#'
+#' @description Addin to notify on used/loaded libraries
+#'
+#' @export
+#'
+notify_libraries_addin <- function() {
+
+  context <- rstudioapi::getActiveDocumentContext()
+  text <- unlist(context$contents)
+  rstudioapi::insertText(notify_libraries(text))
+}
+
 #' Notify on IO components in the code
 #'
 #' @param text String
@@ -45,7 +58,53 @@ notify_io <- function(text) {
       re2r::re2_extract(content, reg_patterns) %>%
       re2r::re2_replace("\\(", "")
     file  <- noquote(re2r::re2_extract(content, in_quotes))
-    matched <- append(matched, paste0("# line ", line, ":\t", cmd, "\t", file, sep = ""))
+    matched <- append(matched, paste0("# line ", line, ":\t", cmd, "\t", file))
+  }
+
+  return(paste(matched, collapse = "\n"))
+}
+
+#' Detect and Display Libraries Used Throughout the Script.
+#'
+#' @param text String
+#'
+#' @return String
+#' @export
+#'
+#' @examples
+#' cat(
+#'   notify_libraries(
+#'     'require(dplyr)
+#'     library(magrittr)
+#'     stringr::str_replace("1", "ONE")'))
+#'
+notify_libraries <- function(text) {
+
+  # const
+  EXPLICIT_GROUP <- 3L
+
+  # object to store libraries matches
+  matched <- list()
+
+  # unlist and trim whitespaces
+  txt <- unlist(stringr::str_split(text, pattern = "\n"))
+  txt <- stringr::str_trim(txt)
+
+  # lines with explicit or implicit library usage
+  matched_lines <- union(
+    which(re2r::re2_detect(string = txt, pattern = library_explicit)),
+    which(re2r::re2_detect(string = txt, pattern = library_implicit))
+  )
+
+  # extract line No. and library name
+  for (line in matched_lines) {
+    content <- txt[line]
+    lib <-
+      re2r::re2_extract(content, library_implicit) %>%
+      re2r::re2_replace("\\:\\:", "")
+    if (is.na(lib))
+      lib <- re2r::re2_match(content, library_explicit)[[EXPLICIT_GROUP]]
+    matched <- append(matched, paste0("# line ", line, ": ", lib))
   }
 
   return(paste(matched, collapse = "\n"))
--- tests/testthat/test_notifier.R
+++ tests/testthat/test_notifier.R
@@ -2,10 +2,16 @@ context("I/O Notifications")
 library(alphabetter)
 
 # test strings:
-#   1. no read/writes at all
-#   2. read/write with single quotes
-#   3. read/write with double quotes
-
+#
+#   I/O artifacts:
+#     1. no read/writes at all
+#     2. read/write with single quotes
+#     3. read/write with double quotes
+#
+#   libraries detection:
+#     4. no libraries at all
+#     5. explicit patterns library(.*), require(.*)
+#     6. implicit pattern .*::some_func
 
 # {1}
 no_rw <-
@@ -29,9 +35,27 @@ double_quotes <-
    head(df)
    write_csv(head(df), "head_iris.csv")'
 
+# {4}
+no_libs <-
+  'malformed library(1234)
+   malformed require(a.)
+   one colon only usethis:pkgdown()'
+
+# {5}
+explicit_libs <-
+  'library(dplyr)
+   require(magrittr)'
+
+# {6}
+implicit_libs <-
+  'dplyr::select(cars, everything())
+   ggplot2::ggplot() +
+     theme_bw()'
+
 test_that("no false alarms", {
   expect_equal(notify_io(no_rw), "")
   expect_equal(notify_io(no_rw2), "")
+  expect_equal(notify_libraries(no_libs), "")
 })
 
 test_that("single quotes", {
@@ -42,4 +66,14 @@ test_that("single quotes", {
 test_that("double quotes", {
   expect_equal(notify_io(double_quotes),
                '# line 1:\tread_csv\t"iris.csv"\n# line 3:\twrite_csv\t"head_iris.csv"')
+})
+
+test_that("explicit libraries", {
+  expect_equal(notify_libraries(explicit_libs),
+               '# line 1: dplyr\n# line 2: magrittr')
+})
+
+test_that("implicit libraries", {
+  expect_equal(notify_libraries(implicit_libs),
+               '# line 1: dplyr\n# line 2: ggplot2')
 })
